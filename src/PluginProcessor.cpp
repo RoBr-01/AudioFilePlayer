d0b88b5fca4584ab3fba135381c6a31761bd3416
RoBr <robinbreugelmans01@gmail.com>
2025-07-21 20:37:13 +0200

    basic set-up

--- src/PluginProcessor.cpp
+++ src/PluginProcessor.cpp
@@ -1,111 +1,141 @@
-// JUCE
-#include "PluginProcessor.hpp"
+/*
+  ==============================================================================
 
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.hpp"
 #include "PluginEditor.hpp"
 
-NewProjectAudioProcessor::NewProjectAudioProcessor()
+//==============================================================================
+AudioFilePlayerAudioProcessor::AudioFilePlayerAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-    : AudioProcessor(
-          BusesProperties()
-#if !JucePlugin_IsMidiEffect
-#if !JucePlugin_IsSynth
-              .withInput("Input", juce::AudioChannelSet::stereo(), true)
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
 #endif
-              .withOutput("Output", juce::AudioChannelSet::stereo(), true)
-#endif
-      )
-#endif
+{
+    formatManager.registerBasicFormats();
+    directoryScannerBackgroundThread.startThread (juce::Thread::Priority::normal);
+}
+
+AudioFilePlayerAudioProcessor::~AudioFilePlayerAudioProcessor()
 {
 }
 
-NewProjectAudioProcessor::~NewProjectAudioProcessor() {}
-
-const juce::String NewProjectAudioProcessor::getName() const {
-    return "MHRTF";
+//==============================================================================
+const juce::String AudioFilePlayerAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
 }
 
-bool NewProjectAudioProcessor::acceptsMidi() const {
-#if JucePlugin_WantsMidiInput
+bool AudioFilePlayerAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
     return true;
-#else
+   #else
     return false;
-#endif
+   #endif
 }
 
-bool NewProjectAudioProcessor::producesMidi() const {
-#if JucePlugin_ProducesMidiOutput
+bool AudioFilePlayerAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
     return true;
-#else
+   #else
     return false;
-#endif
+   #endif
 }
 
-bool NewProjectAudioProcessor::isMidiEffect() const {
-#if JucePlugin_IsMidiEffect
+bool AudioFilePlayerAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
     return true;
-#else
+   #else
     return false;
-#endif
+   #endif
 }
 
-double NewProjectAudioProcessor::getTailLengthSeconds() const {
+double AudioFilePlayerAudioProcessor::getTailLengthSeconds() const
+{
     return 0.0;
 }
 
-int NewProjectAudioProcessor::getNumPrograms() {
-    return 1;
+int AudioFilePlayerAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
 }
 
-int NewProjectAudioProcessor::getCurrentProgram() {
+int AudioFilePlayerAudioProcessor::getCurrentProgram()
+{
     return 0;
 }
 
-void NewProjectAudioProcessor::setCurrentProgram(int index) {}
+void AudioFilePlayerAudioProcessor::setCurrentProgram (int index)
+{
+}
 
-const juce::String NewProjectAudioProcessor::getProgramName(int index) {
+const juce::String AudioFilePlayerAudioProcessor::getProgramName (int index)
+{
     return {};
 }
 
-void NewProjectAudioProcessor::changeProgramName(int index,
-                                                 const juce::String& newName) {}
+void AudioFilePlayerAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
 
-// pre-playback initialisation
-void NewProjectAudioProcessor::prepareToPlay(double sampleRate,
-                                             int samplesPerBlock) {}
+//==============================================================================
+void AudioFilePlayerAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+    transportSource.prepareToPlay(samplesPerBlock, sampleRate);
+}
 
-// post-playback clean-up
-void NewProjectAudioProcessor::releaseResources() {}
+void AudioFilePlayerAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool NewProjectAudioProcessor::isBusesLayoutSupported(
-    const BusesLayout& layouts) const {
-#if JucePlugin_IsMidiEffect
-    juce::ignoreUnused(layouts);
+bool AudioFilePlayerAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
     return true;
-#else
+  #else
     // This is the place where you check if the layout is supported.
     // In this template code we only support mono or stereo.
     // Some plugin hosts, such as certain GarageBand versions, will only
     // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono() &&
-        layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
     // This checks if the input layout matches the output layout
-#if !JucePlugin_IsSynth
+   #if ! JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
-#endif
+   #endif
 
     return true;
-#endif
+  #endif
 }
 #endif
 
-void NewProjectAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer,
-                                            juce::MidiBuffer& midiMessages) {
+void AudioFilePlayerAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
     juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels = getTotalNumInputChannels();
+    auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
     // In case we have more outputs than inputs, this code clears any output
@@ -115,46 +145,90 @@ void NewProjectAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer,
     // when they first compile a plugin, but obviously you don't need to keep
     // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear(i, 0, buffer.getNumSamples());
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel) {
-        auto* channelData = buffer.getWritePointer(channel);
-
-        // ..do something to the data...
+        buffer.clear (i, 0, buffer.getNumSamples());
+    
+    ReferencedTransportSourceData::Ptr ptr;
+    while( fifo.pull(ptr) )
+    {
+        ;
     }
+    
+    if( ptr != nullptr )
+    {
+        pool.add(activeSource);
+        activeSource = ptr;
+        transportSource.stop();
+        transportSource.setSource(activeSource->currentAudioFileSource.get(),
+                                  32768,
+                                  &directoryScannerBackgroundThread,
+                                  activeSource->audioFileSourceSampleRate);
+        sourceHasChanged.set(true);
+    }
+    
+    AudioSourceChannelInfo asci(&buffer, 0, buffer.getNumSamples());
+    transportSource.getNextAudioBlock(asci);
 }
 
 //==============================================================================
-bool NewProjectAudioProcessor::hasEditor() const {
-    return true;
+bool AudioFilePlayerAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
 }
 
-juce::AudioProcessorEditor* NewProjectAudioProcessor::createEditor() {
-    return new NewProjectAudioProcessorEditor(*this);
+juce::AudioProcessorEditor* AudioFilePlayerAudioProcessor::createEditor()
+{
+    return new AudioFilePlayerAudioProcessorEditor (*this);
 }
 
 //==============================================================================
-void NewProjectAudioProcessor::getStateInformation(
-    juce::MemoryBlock& destData) {
+void AudioFilePlayerAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+    if( activeSource != nullptr )
+    {
+        refreshCurrentFileInAPVTS(apvts, activeSource->currentAudioFile);
+        
+        juce::MemoryOutputStream mos(destData, true);
+        apvts.state.writeToStream(mos);
+    }
 }
 
-void NewProjectAudioProcessor::setStateInformation(const void* data,
-                                                   int sizeInBytes) {
-    // You should use this method to restore your parameters from this memory
-    // block, whose contents will have been created by the getStateInformation()
-    // call.
+void AudioFilePlayerAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+    if( tree.isValid() )
+    {
+        apvts.replaceState(tree);
+        if( auto url = apvts.state.getProperty("CurrentFile", {});
+           url != var() )
+        {
+            File file( url.toString() );
+            jassert(file.existsAsFile());
+            if( file.existsAsFile() )
+            {
+                juce::URL path( file );
+                transportSourceCreator.requestTransportForURL(path);
+            }
+        }
+    }
 }
 
-// This creates new instances of the plugin
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter() {
-    return new NewProjectAudioProcessor();
+AudioProcessorValueTreeState::ParameterLayout AudioFilePlayerAudioProcessor::createParameterLayout()
+{
+    AudioProcessorValueTreeState::ParameterLayout layout;
+    
+    using namespace Params;
+//    const auto& paramNames = GetParamNames();
+    
+    return layout;
+}
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new AudioFilePlayerAudioProcessor();
 }
